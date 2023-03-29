6afad565ecae23097f97684694c361fbd89616e6
Hennie Scheepers <hennie.104@hotmail.com>
2023-03-29 14:45:33 +0200

    Completed challenge 2 of IWA-2

---
+++ challenge-2/script.js
@@ -0,0 +1,7 @@
+// Have to import roles needed
+import { role as johannesRole} from "./johannes.js";
+import { role as alexRole } from "./alex.js";
+import { role as nwabisaRole} from "./nwabisa.js"
+
+// .role didn't work so created new variables for each role
+console.log('Roles:', nwabisaRole + ',', johannesRole + ',', alexRole)
